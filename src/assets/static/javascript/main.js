cbc446518ad377ff088bcf4a81250b81b6f0cf03
Fr3shlama <fr3sh@nulldev.org>
2018-10-10 08:02:16 +0200

    F*** you Nulldev Lint
    THERE WERE SO MANY ERRORS

--- src/assets/static/javascript/main.js
+++ src/assets/static/javascript/main.js
@@ -1,19 +1,20 @@
-var countDownDate = new Date("Nov 1, 2018 00:00:00").getTime();
+"use strict";
 
-var x = setInterval(function() {
+let countDownDate = new Date("Nov 1, 2018 00:00:00").getTime();
 
-  var now = new Date().getTime();
-  var distance = countDownDate - now; 
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+let x = setInterval(function(){
+    let now = new Date().getTime();
+    let distance = countDownDate - now;
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "JETZT OFFEN";
-  }
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "JETZT OFFEN";
+    }
 }, 1000);
